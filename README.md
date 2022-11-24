9c138a1942e74a351b9bffd506897c61cc69d2ef
Vaibhav Chellani <vaibahvchellani223@gmail.com>
2022-11-24 17:32:26 +0400

    feat: counter-example

---
+++ README.md
@@ -0,0 +1,1 @@
+# DataLayer Usage Examples
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ISocket} from "./interfaces/ISocket.sol";
+
 contract Counter {
     uint256 public number;
+    address public owner;
+    ISocket socket;
+
+    constructor(address _socket) {
+        owner = msg.sender;
+        socket = ISocket(_socket);
+    }
+
+    //
+    // Modifiers
+    //
+    modifier onlyOwner() {
+        require(msg.sender==owner,"no auth");
+        _;
+    }
+    modifier onlySocket() {
+        require(msg.sender==address(socket),"no auth");
+        _;
+    }
+
+    //
+    // Set Socket Config
+    //
+    function setConfig(uint256 _remoteChainSlug, address _remotePlug) external {
+        socket.setPlugConfig(_remoteChainSlug, _remotePlug, "FAST");
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
-    function increment() public {
-        number++;
+    function setNumber(uint256 newNumber, uint256 chainSlug) public payable {
+        socket.outbound{value: msg.value}(chainSlug, 1000000, abi.encode(newNumber));
+    }
+
+    function inbound(bytes calldata payload_) external payable onlySocket { 
+        uint256 newNumber = abi.decode(payload_, (uint256));
+        setNumber(newNumber);
+    }
+
+    function removeOwner() external onlyOwner() {
+        owner = address(0);
     }
 }
