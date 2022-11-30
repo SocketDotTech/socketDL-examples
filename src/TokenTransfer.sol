426165559bf388ecfac6365ccc4452c5c550a8fe
Aniket Sharma <aniket965.as@gmail.com>
2022-12-01 04:27:04 +0530

    feat: change it to plug base

--- src/TokenTransfer.sol
+++ src/TokenTransfer.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.13;
 
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./interfaces/IPlug.sol";
+import {PlugBase} from "./templates/PlugBase.sol";
 import "./interfaces/ISocket.sol";
 import "./utils/Ownable.sol";
 
@@ -15,8 +15,8 @@ interface SpokePool {
         uint64 relayerFeePct,
         uint32 quoteTimestamp
     ) external payable;
-    function numberOfDeposits() external view returns (uint32);
 
+    function numberOfDeposits() external view returns (uint32);
 }
 
 struct Transfer {
@@ -25,27 +25,30 @@ struct Transfer {
     bool isDone;
 }
 
-contract TokenTransfer is IPlug, Ownable(msg.sender) {
+contract TokenTransfer is PlugBase {
     using SafeERC20 for IERC20;
 
-    // immutables
-    address private immutable socket;
     address public token;
     uint64 public relayerFeePct;
     uint256 public chainId;
     mapping(string => Transfer) public transfers;
 
     SpokePool public immutable spokePool;
-    event Deposit(uint256 toChainId, address recipient, uint256 amount, string depositId);
-      event FullFilled(address recipient, uint256 amount, string depositId);
-      event FullFilledFailed(address recipient, uint256 amount, string depositId);
+    event Deposit(
+        uint256 toChainId,
+        address recipient,
+        uint256 amount,
+        string depositId
+    );
+    event FullFilled(address recipient, uint256 amount, string depositId);
+    event FullFilledFailed(address recipient, uint256 amount, string depositId);
+
     constructor(
         address socket_,
         address token_,
         address spokePool_,
         uint64 relayerFeePct_
-    ) {
-        socket = socket_;
+    ) PlugBase(socket_) {
         token = token_;
         spokePool = SpokePool(spokePool_);
         relayerFeePct = relayerFeePct_;
@@ -61,7 +64,7 @@ contract TokenTransfer is IPlug, Ownable(msg.sender) {
         uint256 amount_,
         uint256 remoteChainSlug_,
         uint256 msgGasLimit_
-    ) external payable  {
+    ) external payable {
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount_);
         IERC20(token).safeIncreaseAllowance(address(spokePool), amount_);
         uint32 numberOfDeposits = spokePool.numberOfDeposits();
@@ -74,15 +77,16 @@ contract TokenTransfer is IPlug, Ownable(msg.sender) {
             relayerFeePct,
             quoteTimestamp
         );
-        
-        string memory depositId = string(abi.encodePacked(numberOfDeposits, chainId));
+
+        string memory depositId = string(
+            abi.encodePacked(numberOfDeposits, chainId)
+        );
         emit Deposit(remoteChainSlug_, receiver_, amount_, depositId);
         bytes memory payload = abi.encode(receiver_, amount_, depositId);
-        _outbound(remoteChainSlug_, msgGasLimit_, payload);
-
+        outbound(remoteChainSlug_, msgGasLimit_, msg.value, payload);
     }
 
-   function rescueFunds(
+    function rescueFunds(
         address _token,
         address _userAddress,
         uint256 _amount
@@ -90,74 +94,42 @@ contract TokenTransfer is IPlug, Ownable(msg.sender) {
         IERC20(_token).safeTransfer(_userAddress, _amount);
     }
 
-    function rescueEther(
-        address payable _userAddress,
-        uint256 _amount
-    ) external onlyOwner {
+    function rescueEther(address payable _userAddress, uint256 _amount)
+        external
+        onlyOwner
+    {
         _userAddress.transfer(_amount);
     }
 
+    function _receiveInbound(bytes memory payload_)
+        internal
+        virtual
+        override
+    {
+        (address receiver, uint256 amount, string memory depositId) = abi
+            .decode(payload_, (address, uint256, string));
 
-    function inbound(
-        bytes calldata payload_
-    ) external payable override {
-        require(msg.sender == socket, "TokenTransfer: Invalid Socket");
-        (address receiver, uint256 amount, string memory depositId) = abi.decode(
-            payload_,
-            (address, uint256, string)
-        );
-
-        try  IERC20(token).transfer(receiver, amount) {
-             transfers[depositId] = Transfer(receiver, amount, true);
+        try IERC20(token).transfer(receiver, amount) {
+            transfers[depositId] = Transfer(receiver, amount, true);
             emit FullFilled(receiver, amount, depositId);
         } catch {
-             transfers[depositId] = Transfer(receiver, amount, false);
-             emit FullFilledFailed(receiver, amount, depositId);
+            transfers[depositId] = Transfer(receiver, amount, false);
+            emit FullFilledFailed(receiver, amount, depositId);
         }
-     
-  
-    }
-    
-    function retryClaim(
-        string memory depositId
-    ) external payable  {
-        Transfer memory transfer = transfers[depositId];
-        require(transfer.recipient != address(0), "TokenTransfer: Invalid depositId");
-        require(transfer.isDone == false, "TokenTransfer: Already claimed");
-        _transfer(transfer.recipient, transfer.amount);
-        transfers[depositId].isDone =  true;
     }
 
-    function _outbound(
-        uint256 targetChain_,
-        uint256 msgGasLimit_,
-        bytes memory payload_
-    ) private {
-        ISocket(socket).outbound{value: msg.value}(
-            targetChain_,
-            msgGasLimit_,
-            payload_
+    function retryClaim(string memory depositId) external payable {
+        Transfer memory transfer = transfers[depositId];
+        require(
+            transfer.recipient != address(0),
+            "TokenTransfer: Invalid depositId"
         );
+        require(transfer.isDone == false, "TokenTransfer: Already claimed");
+        _transfer(transfer.recipient, transfer.amount);
+        transfers[depositId].isDone = true;
     }
 
     function _transfer(address receiver_, uint256 amount_) private {
         IERC20(token).transfer(receiver_, amount_);
     }
-
-    // settings
-    function setSocketConfig(
-        uint256 remoteChainSlug,
-        address remotePlug,
-        string calldata integrationType
-    ) external onlyOwner {
-        ISocket(socket).setPlugConfig(
-            remoteChainSlug,
-            remotePlug,
-            integrationType
-        );
-    }
-
-
-
-
 }
