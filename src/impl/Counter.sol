41c74af82ad34b01a67ffbd9edef1df8639fde14
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-03-01 20:34:39 +0530

    feat: update folder structure

--- src/impl/Counter.sol
+++ src/impl/Counter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
-import {PlugBase} from "./templates/PlugBase.sol";
+import {PlugBase} from "../base/PlugBase.sol";
 
 contract Counter is PlugBase {
     uint256 public number;
